7b35b7689cff3aef61eb814fa7e8cc2b3dce545a
Jarrod Medrano <jmedran@gmail.com>
2023-12-14 09:02:57 -0600

    change send email

--- pages/api/send-email.js
+++ pages/api/send-email.js
@@ -11,45 +11,16 @@ const msg = {
   subject: "Contact from jarrodmedrano.com",
   html: `Message from ${email}: ${message}`,
 };
-sgMail
-  .send(msg)
-  .then(() => {
-    console.log("Email sent");
-  })
-  .catch((error) => {
-    console.error(error);
-  });
 
 const sendEmail = async ({ name, email, message }) => {
-  await fetch(SENDGRID_API, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${SENDGRID_API_KEY}`,
-    },
-    body: JSON.stringify({
-      personalizations: [
-        {
-          to: [
-            {
-              email: "jarrod@jarrodmedrano.com",
-            },
-          ],
-          subject: "Contact from jarrodmedrano.com",
-        },
-      ],
-      from: {
-        email: "jarrod@jarrodmedrano.com",
-        name: name,
-      },
-      content: [
-        {
-          type: "text/html",
-          value: `Message from ${email}: ${message}`,
-        },
-      ],
-    }),
-  });
+  sgMail
+    .send(msg)
+    .then(() => {
+      console.log("Email sent");
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 };
 
 export { sendEmail };
