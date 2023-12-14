333dfb692ac5d265b87d4bdd99232c71e99e766b
Jarrod Medrano <jmedran@gmail.com>
2023-12-14 09:06:06 -0600

    rename path

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -22,7 +22,7 @@ const Contact = () => {
     setMessage("");
 
     try {
-      await fetch("/api/send-email", {
+      await fetch("/api", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
